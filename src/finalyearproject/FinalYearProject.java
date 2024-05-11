0790fc22c6ac5055533d5fe3529faef6cb74572f
Yahya Kassab <ya7yakassab@gmail.com>
2024-05-11 19:22:11 +0300

    Java student grade

---
+++ src/finalyearproject/FinalYearProject.java
@@ -0,0 +1,50 @@
+package finalyearproject;
+
+import javax.swing.*;
+
+public class FinalYearProject {
+
+    public static void main(String[] args) {
+
+        try {
+            // Splash for the first screen
+            // good-looking visual loading screen
+            Splash spl = new Splash();
+            spl.setVisible(true);
+            for (int i = 0; i <= 100; i++) {
+                // set the sleep time of the loader
+                Thread.sleep(45);
+                spl.loadingNum.setText("Loading..." + Integer.toString(i) + " %");
+                spl.jProgressBarload.setValue(i);
+                // open grading panel if loading in the progress bar is over
+                if (i == 100) {
+                    JOptionPane.showMessageDialog(null, "Completed. Click OK to continue.");
+
+                    // Our main app is showed here when the loading is done
+                    LoginFrame app = new LoginFrame();
+                    app.setVisible(true);
+                    spl.setVisible(false);
+                    break; // Exit the loop once the login frame is shown
+                } else if (i == 45) {
+                    spl.guides_add.setText("Please wait while setting things in order");
+                    try {
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else if (i == 90) {
+                    try {
+                        // JOptionPane.showMessageDialog(null, " Table has been created successfully");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    spl.guides_add.setText("All data created.");
+                } else if (i == 99) {
+                    spl.guides_add.setText("Enjoy our service! Thanks!!");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/finalyearproject/Splash.java
@@ -0,0 +1,103 @@
+package finalyearproject;
+
+public class Splash extends javax.swing.JFrame {
+
+
+    public Splash() {
+        initComponents();
+        showLoginScreen();
+
+    }
+ private void showLoginScreen() {
+        LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        loadingNum = new javax.swing.JLabel();
+        jProgressBarload = new javax.swing.JProgressBar();
+        guides_add = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        jPanel1.setBackground(new java.awt.Color(153, 255, 255));
+
+        jLabel5.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText(" STUDENT GRADING SYSTEM");
+
+        loadingNum.setFont(new java.awt.Font("Times New Roman", 2, 18)); // NOI18N
+        loadingNum.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        loadingNum.setText("99%");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jProgressBarload, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(guides_add, javax.swing.GroupLayout.PREFERRED_SIZE, 539, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(112, 112, 112)
+                        .addComponent(loadingNum, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 447, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(67, 67, 67)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(83, 83, 83)
+                .addComponent(loadingNum, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(jProgressBarload, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(guides_add, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Splash().setVisible(true);
+            }
+        });
+    }
+   
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JLabel guides_add;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    public javax.swing.JProgressBar jProgressBarload;
+    public javax.swing.JLabel loadingNum;
+    // End of variables declaration//GEN-END:variables
+}
